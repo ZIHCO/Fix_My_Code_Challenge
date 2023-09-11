830617520b02cfa8d304bfdcbcad1791550c9b37
ZIHCO <chizobajames21@gmail.com>
2023-09-11 19:13:55 +0100

    done

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -64,11 +64,11 @@ class Square(object):
     def __str__(self):
         """return string representation of a square object"""
         return "{}/{}".format(self.width, self.height)
-
+"""
 
 if __name__ == "__main__":
 
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.PermiterOfMySquare())"""
