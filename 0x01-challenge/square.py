f851275f9a2b200a2d76b919479f62a608ce74f2
ZIHCO <chizobajames21@gmail.com>
2023-09-11 19:18:37 +0100

    done

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -68,7 +68,7 @@ class Square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=9, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
