a1603ecf4c419c535c69074ee10135ff1ebf8824
ZIHCO <chizobajames21@gmail.com>
2023-09-12 10:51:52 +0100

    done

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -38,3 +38,8 @@ if __name__ == "__main__":
     print(s)
     print(s.area_of_my_square())
     print(s.perimeter_of_my_square())
+
+    s = Square(width="s", height=5)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.perimeter_of_my_square())
